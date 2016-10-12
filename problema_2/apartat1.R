d39f83d6bc20668d200b7bb2324998bbcc395cfa
jmigual <j.marce.igual@gmail.com>
2016-10-12 12:48:58 +0200

    Added solution 1,2,3 to script 1,2,3

--- problema_2/apartat1.R
+++ problema_2/apartat1.R
@@ -1,32 +1,48 @@
+# Netegem totes les variàbles anteriors
+rm(list=ls())
 
-v1 <- matrix(c(4,2,2,3,4,1,4,3,6,4),nrow=2,byrow = TRUE)
-(v1)
+# Útil per imprimir paràmetres per pantalla
+printf <- function(...) { cat(sprintf(...), "\n") }
 
-(mean(v1[1,]))
-(mean(v1[2,]))
+within_scatter <- function(mat, m) {
+  # S_k = SUM(n)[(x_n - m_m)(x_n - m_-)T
+  # Inicialitzem la matriu S_k
+  Sk <- matrix(0, nrow(mat), nrow(mat))
+  for (i in 1:ncol(mat)) {
+    vaux = mat[,i] - m
+    temp_matrix <- vaux %*% t(vaux)
+    Sk <- Sk + temp_matrix
+  }
+  return(Sk)
+}
 
-vm1 <- matrix(c(mean(v1[1,]),mean(v1[2,])))
+# Afegim les dades del problema per columnes
+v1 <- matrix(c(c(4, 1), c(2, 4), c(2, 3), c(3, 6), c(4, 4)), nrow=2)
+v2 <- matrix(c(c(9, 10), c(6, 8), c(9, 5), c(8, 7), c(10, 8)), nrow=2)
 
-(vm1)
+# Apartat 1 Calcular m1 i m2
+m1 <- matrix(c(mean(v1[1,]), mean(v1[2,])))
+m2 <- matrix(c(mean(v2[1,]), mean(v2[2,])))
 
-vx1 <- c(4,2,2,3,4)
-vy1 <- c(1,4,3,6,4)
+printf("m1")
+print(m1)
 
-vx2 <- c(9,6,9,8,10)
-vy2 <- c(10,8,5,7,8)
+printf("m2")
+print(m2)
 
-mx1 <- mean(vx1)
-my1 <- mean(vy1)
+# Apartat 2 Calcular S1, S2 i SW
+S1 <- within_scatter(v1, m1)
+S2 <- within_scatter(v2, m2)
+SW <- S1 + S2
+printf("S1:")
+print(S1)
+printf("S2:")
+print(S2)
+printf("SW:")
+print(SW)
 
-mx2 <- mean(vx2)
-my2 <- mean(vy2)
-
-(mx1)
-(my1)
-
-(mx2)
-(my2)
-
-# S_w = SUM(n)[(x_n - m_+)(x_n - m_+)T] + SUM(n)[(x_n - m_m)(x_n - m_-)T]
-
-Sw <- sum((vx1 - mx1))
+# Apartat 3 Calcular SB
+m21 <- m2 - m1
+SB <- m21 %*% t(m21)
+printf("SB:")
+print(SB)

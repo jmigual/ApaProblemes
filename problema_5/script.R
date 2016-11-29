402095c5f132b4098239c2ba972b376ebe679c80
Lluc <lluc.bove.canals@gmail.com>
2016-11-29 21:52:44 +0100

    alguns canvis per simplificar i arreglar errors

--- problema_5/script.R
+++ problema_5/script.R
@@ -4,14 +4,16 @@ tot <- read.table(file.choose())
 lletres = tot[1:35]
 
 canviaLletra <- function(lletra) {
-  index = c(floor(runif(rpois(1, 1.01), 1, 35)))
+  lletra <- as.integer(lletra[1:35])
+  index = c(sample.int(35,rpois(1, 1.01)))
   if (length(index) == 0) return(lletra)
-  
   lletra[index] = 1 - lletra[index]
   lletra
 }
 
 sobrePP <- function(N, tot) {
-  data = tot[c(floor(runif(N, 1, 26))),]
-  lapply(data, canviaLletra)
+  data <- tot[sample.int(26,N,replace=T),]
+  data[,1:35] <- t(apply(data, 1,canviaLletra))
+  rownames(data) <- NULL
+  data
 }

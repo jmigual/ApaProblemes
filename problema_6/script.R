441cfceccc4fd16112f66fe4b5081bc77bcccdb5
RaulPROP <raul_ibpe@hotmail.com>
2016-12-19 21:41:02 +0100

    comentaris

--- problema_6/script.R
+++ problema_6/script.R
@@ -1,3 +1,24 @@
+
+##########################
+### PROBLEMA DE GRUP 6 ###
+##########################
+### Raúl Ibáñez        ###
+### Lluc Bové          ###
+### Aleix Trasserra    ###
+### Joan  Marcè        ###
+##########################
+
+## La funció smiley() genera problemes de classificacio de 4 classes en
+## dos dimensions, seguint una certa estructura, que es composa de dos
+## ulls gaussians, un nas trapezoidal i una boca parabolica (amb soroll
+## gaussia vertical).
+
+## Es demana:
+### 1) Carregueu les dades APA-TEMA8-P2.Rdata. Heu de crear tambe un
+###    conjunt de test de mida N.test amb les mateixes caracteristiques
+###    de soroll.
+
+# Creem les dades de test amb les mateixes característiques de soroll.
 library(mlbench)
 N.train <- 1000
 N.test <- 10000
@@ -7,14 +28,21 @@ plot(p)
 
 test <- as.data.frame(p)
 
-load(file.choose())
+# (per poder tractar tots amb les mateixes dades tenim unes guardades)
+load(file.choose()) # Carregar dades de train
+load(file.choose()) # Carregar dades de test
+
+### 2) Dissenyeu un model de prediccio basat en la maquina de vectors
+###    suport -rutina ksvm() del paquet kernlab- per predir la classe.
 
 library(kernlab)
+
 
 linear <- ksvm(classes ~., data = smiley, kernel = "vanilladot", C = 6, cross = 10)
 
 cList <- c(0.01,0.1,1,10,100,1000)
 
+# Aquesta funcio fa crides a ksvm i busca el millor model.
 polinomKsvm = function(C, kernel, degree) {
   auxCV = 0
   for (i in 1:10){
@@ -24,6 +52,8 @@ polinomKsvm = function(C, kernel, degree) {
   auxCV <- auxCV/10
   return(auxCV)
 }
+
+## Busquem el millor model lineal ##
 
 minCV <- Inf
 minC <- Inf
@@ -37,7 +67,8 @@ for (C in cList) {
 minCV
 minC
 
-###################### QUADRATIC SVM #########################################
+## Busquem el millor model quadratic ##
+
 minCV2 <- Inf
 minC2 <- Inf
 for (C in cList) {
@@ -50,9 +81,8 @@ for (C in cList) {
 minCV2
 minC2
 
+## Busquem el millor model cubic ##
 
-
-###################### CUBIC SVM #########################################
 minCV3 <- Inf
 minC3 <- Inf
 for (C in cList) {
@@ -65,8 +95,8 @@ for (C in cList) {
 minCV3
 minC3
 
-
-###################### RBF SVM #########################################
+## Busquem el millor model per RBF ##
+# (Triga bastant)
 minCV4 <- Inf
 minC4 <- Inf
 minS4 <- Inf
@@ -95,17 +125,41 @@ minCV4
 minC4
 minS4
 
+# El model amb l'error minim es el model amb RBF
+
 model = ksvm(classes ~., data = smiley, kernel = "rbfdot", C = minC4, sigma = minS4)
 
 result = predict(model)
 
+# Calculem l'error de training
+
 100*(1 - sum(diag(table(result, smiley$classes)))/length(result))
+
+# Error de training = 4.9%
 
 result.test = predict(model, test)
 table(result.test, test$classes)
+
+# Calculem l'error de test
+
 100*(1 - sum(diag(table(result.test, test$classes)))/length(result.test))
 
+# Error de test = 6.72%
+
+plot(test$x.x4,y = test$x.V2)
+plot(smiley$x.x4,y=smiley$x.V2)
+
+# Plots per veure les diferencies
+
 par(mfrow=c(1,2))
-plot(x = test$x.x4, y = test$x.V2, col = result.test)
-plot(x = test$x.x4, y = test$x.V2, col = test$classes)
-par(mfrow=c(1,1))
+plot(x = test$x.x4, y = test$x.V2, col = result.test, main = 'Dades de prediccio',xlab = '',ylab = '',xaxt='n',yaxt = 'n')
+plot(x = test$x.x4, y = test$x.V2, col = test$classes, main = 'Dades originals',xlab = '', ylab = '',xaxt='n',yaxt = 'n')
+
+### Heu d'acabar donant un sol model final i una estimacio honesta de
+### com prediu.
+
+# El nostre model final es el model per rbf amb els valors optims calculats, es a dir:
+model = ksvm(classes ~., data = smiley, kernel = "rbfdot", C = minC4, sigma = minS4)
+# Honestament, aquest model prediu força bé ja que s'equivoca en punts que es superposen, cosa que es lògic
+# donat que si no s'equivoqués seria dolent, ja que no estaria donant la probabilitat més alta.
+
